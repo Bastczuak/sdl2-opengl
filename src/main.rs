7d08e032db0ebbf7679d0ecbd4056528d8baa7ef
Sebastian Richter <sebastian.richter@paessler.com>
2021-11-02 14:11:04 +0100

    -ebos.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+#[allow(clippy::all)]
 mod gl {
-    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+  include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 
 use gl::types::*;
-use sdl2::event::Event;
+use sdl2::event::{Event, WindowEvent};
 use sdl2::keyboard::Keycode;
 use sdl2::video::GLProfile;
 use std::ffi::CString;
@@ -11,224 +12,227 @@ use std::ops::Deref;
 use std::ptr;
 
 pub struct Gl {
-    inner: std::rc::Rc<gl::Gl>,
+  inner: std::rc::Rc<gl::Gl>,
 }
 
 impl Gl {
-    fn load_with<F>(load_fn: F) -> Self
+  fn load_with<F>(load_fn: F) -> Self
     where
-        F: FnMut(&'static str) -> *const GLvoid,
-    {
-        Self {
-            inner: std::rc::Rc::new(gl::Gl::load_with(load_fn)),
-        }
+      F: FnMut(&'static str) -> *const GLvoid,
+  {
+    Self {
+      inner: std::rc::Rc::new(gl::Gl::load_with(load_fn)),
     }
+  }
 }
 
 impl Deref for Gl {
-    type Target = gl::Gl;
+  type Target = gl::Gl;
 
-    fn deref(&self) -> &Self::Target {
-        &self.inner
-    }
+  fn deref(&self) -> &Self::Target {
+    &self.inner
+  }
 }
 
 #[rustfmt::skip]
-const TRIANGLE_VERTICIES: [GLfloat; 18] = [
-  // positions xyz  // colors rgb
-  0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-  -0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-  0.0, 0.5, 0.0, 0.0, 0.0, 1.0
+const TRIANGLE_VERTICIES: [f32; 12] = [
+  0.5, 0.5, 0.0,  // top right
+  0.5, -0.5, 0.0,  // bottom right
+  -0.5, -0.5, 0.0,  // top left
+  -0.5, 0.5, 0.0,  // bottom left
 ];
 
-const VERTEX_SHADER: &'static str = r#"
+const TRIANGLE_INDICIES: [u32; 6] = [
+  0, 1, 3,
+  1, 2, 3
+];
+
+const VERTEX_SHADER: &str = r#"
 #version 330 core
 
 layout (location = 0) in vec3 Position;
-layout (location = 1) in vec3 Color;
-
-out VERTEX_SHADER_OUTPUT {
-  vec3 Color;
-} OUT;
 
 void main() {
   gl_Position = vec4(Position, 1.0);
-  OUT.Color = Color;
 }
 "#;
-const FRAGMENT_SHADER: &'static str = r#"
+const FRAGMENT_SHADER: &str = r#"
 #version 330 core
-
-in VERTEX_SHADER_OUTPUT {
-  vec3 Color;
-} IN;
 
 out vec4 Color;
 
 void main() {
-  Color = vec4(IN.Color, 1.0);
+  Color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
 }
 "#;
 
 unsafe fn create_error_buffer(length: usize) -> CString {
-    let mut buffer = Vec::with_capacity(length + 1);
-    buffer.extend([b' '].iter().cycle().take(length));
-    CString::from_vec_unchecked(buffer)
+  let mut buffer = Vec::with_capacity(length + 1);
+  buffer.extend([b' '].iter().cycle().take(length));
+  CString::from_vec_unchecked(buffer)
 }
 
 fn compile_shader(gl: &gl::Gl, src: &str, kind: GLenum) -> Result<GLuint, String> {
-    unsafe {
-        let shader = gl.CreateShader(kind);
-        let c_str_src = CString::new(src.as_bytes()).unwrap();
-        gl.ShaderSource(shader, 1, &c_str_src.as_ptr(), ptr::null());
-        gl.CompileShader(shader);
-        let mut success = gl::TRUE as GLint;
-        gl.GetShaderiv(shader, gl::COMPILE_STATUS, &mut success);
+  unsafe {
+    let shader = gl.CreateShader(kind);
+    let c_str_src = CString::new(src.as_bytes()).unwrap();
+    gl.ShaderSource(shader, 1, &c_str_src.as_ptr(), ptr::null());
+    gl.CompileShader(shader);
+    let mut success = gl::TRUE as GLint;
+    gl.GetShaderiv(shader, gl::COMPILE_STATUS, &mut success);
 
-        if success == (gl::FALSE as GLint) {
-            let mut len = 0;
-            gl.GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
-            let error = create_error_buffer(len as usize);
-            gl.GetShaderInfoLog(shader, len, ptr::null_mut(), error.as_ptr() as *mut GLchar);
-            return Err(error.to_string_lossy().into_owned());
-        }
-        Ok(shader)
+    if success == (gl::FALSE as GLint) {
+      let mut len = 0;
+      gl.GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
+      let error = create_error_buffer(len as usize);
+      gl.GetShaderInfoLog(shader, len, ptr::null_mut(), error.as_ptr() as *mut GLchar);
+      return Err(error.to_string_lossy().into_owned());
     }
+    Ok(shader)
+  }
 }
 
 fn link_program(
-    gl: &gl::Gl,
-    vertex_shader: GLuint,
-    fragment_shader: GLuint,
+  gl: &gl::Gl,
+  vertex_shader: GLuint,
+  fragment_shader: GLuint,
 ) -> Result<GLuint, String> {
-    unsafe {
-        let program = gl.CreateProgram();
-        gl.AttachShader(program, vertex_shader);
-        gl.AttachShader(program, fragment_shader);
-        gl.LinkProgram(program);
-        let mut success = gl::TRUE as GLint;
-        gl.GetProgramiv(program, gl::LINK_STATUS, &mut success);
+  unsafe {
+    let program = gl.CreateProgram();
+    gl.AttachShader(program, vertex_shader);
+    gl.AttachShader(program, fragment_shader);
+    gl.LinkProgram(program);
+    let mut success = gl::TRUE as GLint;
+    gl.GetProgramiv(program, gl::LINK_STATUS, &mut success);
 
-        if success == (gl::FALSE as GLint) {
-            let mut len = 0;
-            gl.GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
-            let error = create_error_buffer(len as usize);
-            gl.GetProgramInfoLog(program, len, ptr::null_mut(), error.as_ptr() as *mut GLchar);
-            return Err(error.to_string_lossy().into_owned());
-        }
-
-        gl.DetachShader(program, vertex_shader);
-        gl.DetachShader(program, fragment_shader);
-        Ok(program)
+    if success == (gl::FALSE as GLint) {
+      let mut len = 0;
+      gl.GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
+      let error = create_error_buffer(len as usize);
+      gl.GetProgramInfoLog(program, len, ptr::null_mut(), error.as_ptr() as *mut GLchar);
+      return Err(error.to_string_lossy().into_owned());
     }
+
+    gl.DetachShader(program, vertex_shader);
+    gl.DetachShader(program, fragment_shader);
+
+    Ok(program)
+  }
 }
 
 fn main() -> Result<(), String> {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+  let sdl_context = sdl2::init().unwrap();
+  let video_subsystem = sdl_context.video().unwrap();
 
-    let gl_attr = video_subsystem.gl_attr();
-    gl_attr.set_context_profile(GLProfile::Core);
-    gl_attr.set_context_version(3, 3);
+  let gl_attr = video_subsystem.gl_attr();
+  gl_attr.set_context_profile(GLProfile::Core);
+  gl_attr.set_context_version(3, 3);
 
-    let window = video_subsystem
-        .window("Window", 800, 600)
-        .opengl()
-        .position_centered()
-        .build()
-        .unwrap();
+  let window = video_subsystem
+    .window("Window", 800, 600)
+    .opengl()
+    .resizable()
+    .position_centered()
+    .build()
+    .unwrap();
 
-    let _ctx = window.gl_create_context().unwrap();
-    let gl = Gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
+  let _ctx = window.gl_create_context().unwrap();
+  let gl = Gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
 
-    debug_assert_eq!(gl_attr.context_profile(), GLProfile::Core);
-    debug_assert_eq!(gl_attr.context_version(), (3, 3));
+  debug_assert_eq!(gl_attr.context_profile(), GLProfile::Core);
+  debug_assert_eq!(gl_attr.context_version(), (3, 3));
 
-    let vertex_shader = compile_shader(&gl, VERTEX_SHADER, gl::VERTEX_SHADER)?;
-    let fragment_shader = compile_shader(&gl, FRAGMENT_SHADER, gl::FRAGMENT_SHADER)?;
-    let program = link_program(&gl, vertex_shader, fragment_shader)?;
+  let vertex_shader = compile_shader(&gl, VERTEX_SHADER, gl::VERTEX_SHADER)?;
+  let fragment_shader = compile_shader(&gl, FRAGMENT_SHADER, gl::FRAGMENT_SHADER)?;
+  let program = link_program(&gl, vertex_shader, fragment_shader)?;
 
-    let mut vbo = 0; // Vertex Buffer Object
-    let mut vao = 0; // Vertex Array Object
+  let mut vbo = 0; // Vertex Buffer Object
+  let mut vao = 0; // Vertex Array Object
+  let mut ebo = 0; // Element Array Object
 
-    unsafe {
-        gl.GenBuffers(1, &mut vbo);
-        gl.BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl.BufferData(
-            gl::ARRAY_BUFFER,
-            (TRIANGLE_VERTICIES.len() * std::mem::size_of::<GLfloat>()) as GLsizeiptr,
-            TRIANGLE_VERTICIES.as_ptr() as *const GLvoid,
-            gl::STATIC_DRAW,
-        );
-        gl.BindBuffer(gl::ARRAY_BUFFER, 0);
+  unsafe {
+    gl.GenBuffers(1, &mut vbo);
+    gl.GenBuffers(1, &mut ebo);
+    gl.GenVertexArrays(1, &mut vao);
 
-        gl.GenVertexArrays(1, &mut vao);
-        gl.BindVertexArray(vao);
-        gl.BindBuffer(gl::ARRAY_BUFFER, vbo);
+    gl.BindVertexArray(vao);
 
-        gl.UseProgram(program);
-        let pos_attr = gl.GetAttribLocation(program, CString::new("Position").unwrap().into_raw());
-        gl.EnableVertexAttribArray(pos_attr as GLuint);
-        gl.VertexAttribPointer(
-            pos_attr as GLuint,
-            3,
-            gl::FLOAT,
-            gl::FALSE,
-            (6 * std::mem::size_of::<GLfloat>()) as GLint, // offset of each point
-            std::ptr::null(),
-        );
-        let color_attr = gl.GetAttribLocation(program, CString::new("Color").unwrap().into_raw());
-        gl.EnableVertexAttribArray(color_attr as GLuint);
-        gl.VertexAttribPointer(
-            color_attr as GLuint,
-            3,
-            gl::FLOAT,
-            gl::FALSE,
-            (6 * std::mem::size_of::<GLfloat>()) as GLint, // offset of each point
-            (3 * std::mem::size_of::<GLfloat>()) as *const GLvoid, // offset of each color
-        );
-        gl.BindBuffer(gl::ARRAY_BUFFER, 0);
-        gl.BindVertexArray(0);
-    }
+    gl.BindBuffer(gl::ARRAY_BUFFER, vbo);
+    gl.BufferData(
+      gl::ARRAY_BUFFER,
+      (TRIANGLE_VERTICIES.len() * std::mem::size_of::<f32>()) as GLsizeiptr,
+      TRIANGLE_VERTICIES.as_ptr() as *const GLvoid,
+      gl::STATIC_DRAW,
+    );
 
-    unsafe {
-        gl.Viewport(0, 0, 800, 600);
-        gl.ClearColor(0.7, 0.0, 0.8, 1.0);
-    }
+    gl.BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+    gl.BufferData(
+      gl::ELEMENT_ARRAY_BUFFER,
+      (TRIANGLE_INDICIES.len() * std::mem::size_of::<u32>()) as GLsizeiptr,
+      TRIANGLE_INDICIES.as_ptr() as *const GLvoid,
+      gl::STATIC_DRAW,
+    );
 
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    let pos_attr = gl.GetAttribLocation(program, CString::new("Position").unwrap().into_raw());
+    gl.EnableVertexAttribArray(pos_attr as GLuint);
+    gl.VertexAttribPointer(
+      pos_attr as GLuint,
+      3,
+      gl::FLOAT,
+      gl::FALSE,
+      (3 * std::mem::size_of::<f32>()) as GLint, // offset of each point
+      std::ptr::null(),
+    );
+    gl.BindBuffer(gl::ARRAY_BUFFER, 0);
+    gl.BindVertexArray(0);
+    gl.PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
+  }
 
-    'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => {
-                    break 'running;
-                }
-                _ => {}
-            }
+  unsafe {
+    gl.Viewport(0, 0, 800, 600);
+    gl.ClearColor(0.2, 0.3, 0.3, 1.0);
+  }
+
+  let mut event_pump = sdl_context.event_pump().unwrap();
+
+  'running: loop {
+    for event in event_pump.poll_iter() {
+      match event {
+        Event::Quit { .. }
+        | Event::KeyDown {
+          keycode: Some(Keycode::Escape),
+          ..
+        } => {
+          break 'running;
         }
-
-        unsafe {
-            gl.Clear(gl::COLOR_BUFFER_BIT);
-            // Draw a triangle from the 3 vertices
-            gl.BindVertexArray(vao);
-            gl.DrawArrays(gl::TRIANGLES, 0, 3);
-        }
-
-        window.gl_swap_window();
-
-        ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
+        Event::Window { win_event: WindowEvent::Resized(w, h), .. } => unsafe {
+          gl.Viewport(0, 0, w, h);
+        },
+        _ => {}
+      }
     }
 
     unsafe {
-        gl.DeleteProgram(program);
-        gl.DeleteShader(vertex_shader);
-        gl.DeleteShader(fragment_shader);
+      gl.Clear(gl::COLOR_BUFFER_BIT);
+      gl.UseProgram(program);
+      gl.BindVertexArray(vao);
+
+      gl.DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, 0 as *const _);
     }
 
-    Ok(())
+    window.gl_swap_window();
+
+    ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
+  }
+
+  unsafe {
+    gl.DeleteVertexArrays(1, &vao);
+    gl.DeleteBuffers(1, &vbo);
+    gl.DeleteBuffers(1, &ebo);
+    gl.DeleteProgram(program);
+    gl.DeleteShader(vertex_shader);
+    gl.DeleteShader(fragment_shader);
+  }
+
+  Ok(())
 }
