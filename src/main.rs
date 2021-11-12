bf87a20a256995087278e154c4a47f949e4d78c3
Sebastian Richter <sebastian.richter@paessler.com>
2021-11-12 17:06:47 +0100

    -textures.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod gl {
 }
 
 use gl::types::*;
+use image::GenericImageView;
 use sdl2::event::{Event, WindowEvent};
 use sdl2::keyboard::Keycode;
 use sdl2::video::GLProfile;
@@ -34,36 +35,47 @@ impl Deref for Gl {
 }
 
 #[rustfmt::skip]
-const TRIANGLE_VERTICIES: [f32; 12] = [
-  0.5, 0.5, 0.0,  // top right
-  0.5, -0.5, 0.0,  // bottom right
-  -0.5, -0.5, 0.0,  // top left
-  -0.5, 0.5, 0.0,  // bottom left
+const TRIANGLE_VERTICIES: [f32; 20] = [
+  // positions     //texture coords
+  0.5, 0.5, 0.0, 1.0, 1.0,  // top right
+  0.5, -0.5, 0.0, 1.0, 0.0,  // bottom right
+  -0.5, -0.5, 0.0, 0.0, 0.0,  // top left
+  -0.5, 0.5, 0.0, 0.0, 1.0,  // bottom left
 ];
 
-const TRIANGLE_INDICIES: [u32; 6] = [
-  0, 1, 3,
-  1, 2, 3
-];
+const TRIANGLE_INDICIES: [u32; 6] = [0, 1, 3, 1, 2, 3];
 
 const VERTEX_SHADER: &str = r#"
 #version 330 core
 
 layout (location = 0) in vec3 Position;
+layout (location = 1) in vec2 TexCoords;
+
+out VERTEX_SHADER_OUTPUT {
+  vec2 TexCoords;
+} OUT;
 
 void main() {
   gl_Position = vec4(Position, 1.0);
+  OUT.TexCoords = TexCoords;
 }
 "#;
 const FRAGMENT_SHADER: &str = r#"
 #version 330 core
 
+in VERTEX_SHADER_OUTPUT {
+  vec2 TexCoords;
+} IN;
+
 out vec4 Color;
 
 uniform vec3 uColor;
+uniform sampler2D uTexture0;
+uniform sampler2D uTexture1;
+uniform float uMixValue;
 
 void main() {
-  Color = vec4(uColor, 1.0f);
+  Color = mix(texture(uTexture0, IN.TexCoords), texture(uTexture1, IN.TexCoords * vec2(1.0, -1.0)), uMixValue) * vec4(uColor, 1.0);
 }
 "#;
 
@@ -86,7 +98,12 @@ fn compile_shader(gl: &gl::Gl, src: &str, kind: GLenum) -> Result<GLuint, String
       let mut len = 0;
       gl.GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
       let error = create_error_buffer(len as usize);
-      gl.GetShaderInfoLog(shader, len, std::ptr::null_mut(), error.as_ptr() as *mut GLchar);
+      gl.GetShaderInfoLog(
+        shader,
+        len,
+        std::ptr::null_mut(),
+        error.as_ptr() as *mut GLchar,
+      );
       return Err(error.to_string_lossy().into_owned());
     }
     Ok(shader)
@@ -110,7 +127,12 @@ fn link_program(
       let mut len = 0;
       gl.GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
       let error = create_error_buffer(len as usize);
-      gl.GetProgramInfoLog(program, len, std::ptr::null_mut(), error.as_ptr() as *mut GLchar);
+      gl.GetProgramInfoLog(
+        program,
+        len,
+        std::ptr::null_mut(),
+        error.as_ptr() as *mut GLchar,
+      );
       return Err(error.to_string_lossy().into_owned());
     }
 
@@ -150,14 +172,14 @@ fn main() -> Result<(), String> {
   let mut vbo = 0; // Vertex Buffer Object
   let mut vao = 0; // Vertex Array Object
   let mut ebo = 0; // Element Array Object
+  let mut textures = [0; 2];
 
   unsafe {
     gl.GenBuffers(1, &mut vbo);
     gl.GenBuffers(1, &mut ebo);
+    gl.GenTextures(2, textures.as_mut_ptr());
     gl.GenVertexArrays(1, &mut vao);
-
     gl.BindVertexArray(vao);
-
     gl.BindBuffer(gl::ARRAY_BUFFER, vbo);
     gl.BufferData(
       gl::ARRAY_BUFFER,
@@ -165,7 +187,6 @@ fn main() -> Result<(), String> {
       TRIANGLE_VERTICIES.as_ptr() as *const GLvoid,
       gl::STATIC_DRAW,
     );
-
     gl.BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
     gl.BufferData(
       gl::ELEMENT_ARRAY_BUFFER,
@@ -173,28 +194,91 @@ fn main() -> Result<(), String> {
       TRIANGLE_INDICIES.as_ptr() as *const GLvoid,
       gl::STATIC_DRAW,
     );
-
     let pos_attr = gl.GetAttribLocation(program, CString::new("Position").unwrap().into_raw());
-    gl.EnableVertexAttribArray(pos_attr as GLuint);
+    gl.EnableVertexAttribArray(pos_attr as u32);
     gl.VertexAttribPointer(
-      pos_attr as GLuint,
+      pos_attr as u32,
       3,
       gl::FLOAT,
       gl::FALSE,
-      (3 * std::mem::size_of::<f32>()) as GLint, // offset of each point
+      (5 * std::mem::size_of::<f32>()) as i32, // offset of each point
       std::ptr::null(),
+    );
+    let texture_coords_attr =
+      gl.GetAttribLocation(program, CString::new("TexCoords").unwrap().into_raw());
+    gl.EnableVertexAttribArray(texture_coords_attr as u32);
+    gl.VertexAttribPointer(
+      texture_coords_attr as u32,
+      2,
+      gl::FLOAT,
+      gl::FALSE,
+      (5 * std::mem::size_of::<f32>()) as i32, // offset of each point
+      (3 * std::mem::size_of::<f32>()) as *const GLvoid, // offset of each point
     );
     gl.BindBuffer(gl::ARRAY_BUFFER, 0);
     gl.BindVertexArray(0);
-  }
 
-  unsafe {
+    // load a new texture
+    gl.BindTexture(gl::TEXTURE_2D, textures[0]);
+    gl.TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
+    gl.TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+    gl.TexParameteri(
+      gl::TEXTURE_2D,
+      gl::TEXTURE_MIN_FILTER,
+      gl::LINEAR_MIPMAP_LINEAR as i32,
+    );
+    gl.TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+    let img_container = image::open("container.jpeg").unwrap();
+    let (width, height) = img_container.dimensions();
+    gl.TexImage2D(
+      gl::TEXTURE_2D,
+      0,
+      gl::RGB as i32,
+      width as i32,
+      height as i32,
+      0,
+      gl::RGB,
+      gl::UNSIGNED_BYTE,
+      img_container.as_bytes().as_ptr() as *const GLvoid,
+    );
+    gl.GenerateMipmap(gl::TEXTURE_2D);
+
+    // load a new texture
+    gl.BindTexture(gl::TEXTURE_2D, textures[1]);
+    gl.TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
+    gl.TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+    gl.TexParameteri(
+      gl::TEXTURE_2D,
+      gl::TEXTURE_MIN_FILTER,
+      gl::LINEAR_MIPMAP_LINEAR as i32,
+    );
+    gl.TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+    let img_face = image::open("awesomeface.png").unwrap();
+    let (width, height) = img_face.dimensions();
+    gl.TexImage2D(
+      gl::TEXTURE_2D,
+      0,
+      gl::RGBA as i32,
+      width as i32,
+      height as i32,
+      0,
+      gl::RGBA,
+      gl::UNSIGNED_BYTE,
+      img_face.as_bytes().as_ptr() as *const GLvoid,
+    );
+    gl.GenerateMipmap(gl::TEXTURE_2D);
+
+    gl.UseProgram(program);
+    gl.Uniform1i(gl.GetUniformLocation(program, CString::new("uTexture0").unwrap().into_raw()), 0);
+    gl.Uniform1i(gl.GetUniformLocation(program, CString::new("uTexture1").unwrap().into_raw()), 1);
+
     gl.Viewport(0, 0, 800, 600);
     gl.ClearColor(0.2, 0.3, 0.3, 1.0);
   }
 
   let mut event_pump = sdl_context.event_pump().unwrap();
   let timer = sdl_context.timer().unwrap();
+  let mut mix_value = 0.0f32;
 
   'running: loop {
     for event in event_pump.poll_iter() {
@@ -206,7 +290,16 @@ fn main() -> Result<(), String> {
         } => {
           break 'running;
         }
-        Event::Window { win_event: WindowEvent::Resized(w, h), .. } => unsafe {
+        Event::KeyDown { keycode: Some(Keycode::Down), ..} => {
+          mix_value = (mix_value - 0.01).max(0.0)
+        },
+        Event::KeyDown { keycode: Some(Keycode::Up), ..} => {
+          mix_value = (mix_value + 0.01).min(1.0)
+        }
+        Event::Window {
+          win_event: WindowEvent::Resized(w, h),
+          ..
+        } => unsafe {
           gl.Viewport(0, 0, w, h);
         },
         _ => {}
@@ -215,11 +308,21 @@ fn main() -> Result<(), String> {
 
     unsafe {
       gl.Clear(gl::COLOR_BUFFER_BIT);
-      gl.UseProgram(program);
       let seconds = timer.ticks() as f32 / 1000.0;
       let green_color = f32::sin(seconds) / 2.0 + 0.5;
-      let vertex_color_location = gl.GetUniformLocation(program, CString::new("uColor").unwrap().into_raw());
-      gl.Uniform3f(vertex_color_location, 0.0, green_color, 0.0);
+      gl.Uniform3f(
+        gl.GetUniformLocation(program, CString::new("uColor").unwrap().into_raw()),
+        0.0,
+        green_color,
+        0.0,
+      );
+      gl.Uniform1f(gl.GetUniformLocation(program, CString::new("uMixValue").unwrap().into_raw()), mix_value);
+      gl.ActiveTexture(gl::TEXTURE0);
+      gl.BindTexture(gl::TEXTURE_2D, textures[0]);
+      gl.ActiveTexture(gl::TEXTURE1);
+      gl.BindTexture(gl::TEXTURE_2D, textures[1]);
+
+      gl.UseProgram(program);
       gl.BindVertexArray(vao);
       gl.DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, std::ptr::null());
     }
@@ -233,6 +336,7 @@ fn main() -> Result<(), String> {
     gl.DeleteVertexArrays(1, &vao);
     gl.DeleteBuffers(1, &vbo);
     gl.DeleteBuffers(1, &ebo);
+    gl.DeleteBuffers(2, textures.as_ptr());
     gl.DeleteProgram(program);
     gl.DeleteShader(vertex_shader);
     gl.DeleteShader(fragment_shader);
